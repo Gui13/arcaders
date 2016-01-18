da7865be159259c025ae10d886f29b4e58b96ab1
Guillaume BIENKOWSKI <guitreize@gmail.com>
2016-01-18 13:48:20 +0100

    Event abstraction

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,44 @@ extern crate sdl2;
 
 use sdl2::pixels::Color;
 
+
+// #[macro_use] asks the compiler to import the macros defined in the `events`
+// module. This is necessary because macros cannot be namespaced -- macro
+// expansion happens before the concept of namespace even starts to _exist_ in
+// the compilation timeline.
+
+#[macro_use]
 mod events;
-use ::events::Events;
+
+// list of the keyboard events we're interested in.
+// See events.rs for the macro definition.
+struct_events! {
+    keyboard: {
+        key_escape: Escape
+    },
+    else: {
+        quit: Quit { .. }
+    }
+}
+
+use sdl2::render::Renderer;
+
+/// Bundles the Phi abstractions in a single structure which
+/// can be passed easily between functions.
+pub struct Phi<'window> {
+    pub events: Events,
+    pub renderer: Renderer<'window>,
+}
+
+pub enum ViewAction {
+    None,
+    Quit,
+}
+
+pub trait View {
+    fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
+
 
 fn main() {
     println!("Hello, world!");
@@ -12,25 +48,25 @@ fn main() {
     let video = sdl_context.video().unwrap();
 
     let window = video.window("ArcadeRS Shooter", 800, 600)
-    	.position_centered().opengl().build().unwrap();
+                      .position_centered()
+                      .opengl()
+                      .build()
+                      .unwrap();
 
     let mut renderer = window.renderer().accelerated().build().unwrap();
-
-
     let mut events = Events::new(sdl_context.event_pump().unwrap());
 
     loop {
         events.pump();
 
-        if events.quit || events.key_escape {
+        if events.now.key_escape == Some(true) || events.now.quit {
             break;
         }
 
-        renderer.set_draw_color(Color::RGB(0,0,0));
+        renderer.set_draw_color(Color::RGB(0, 0, 0));
         renderer.clear();
         renderer.present();
 
-        thread::sleep_ms(20);
     }
 
 }
